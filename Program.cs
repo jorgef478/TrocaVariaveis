7137a5657b76b5100b7f0e5224c1b11731f3b593
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-09-01 22:06:42 -0300

    Programa finalizado

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("--- Troca de valores ---");
+Console.WriteLine();
+
+Console.Write("Nome 1...: ");
+String nome1 = Console.ReadLine()!;
+Console.Write("Nome 2...: ");
+String nome2 = Console.ReadLine()!;
+Console.Write("Nome 3...: ");
+String nome3 = Console.ReadLine()!;
+Console.Write("Nome 4...: ");
+String nome4 = Console.ReadLine()!;
+Console.WriteLine();
+
+Console.WriteLine("Após trocar os valores temos: ");
+
+Console.WriteLine();
+Console.Write("Nome 1 = ");
+Console.WriteLine(nome4);
+Console.Write("Nome 2 = ");
+Console.WriteLine(nome3);
+Console.Write("Nome 3 = ");
+Console.WriteLine(nome2);
+Console.Write("Nome 4 = ");
+Console.WriteLine(nome1);
